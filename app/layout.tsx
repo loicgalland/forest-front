d0d597eebbc74d1e2b4e0a531f81b3b52f489c10
loicgalland <loicgalland1@gmail.com>
2024-12-17 15:45:32 +0100

    feat(seo): add openGraph metaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,15 @@ import { Header } from "./components/Header";
 import { AuthProvider } from "./services/AuthContext";
 
 export const metadata: Metadata = {
-  title: "Forest",
+  title: "Réservation hébergement - Forest",
   description:
     "Venez réserver votre séjour éco-responsable dans le Gers. Grâce à Forest vous pourrez découvrir un lieu" +
     " préservé et participer à des activités ou a des événements.",
+  openGraph: {
+    title: "Réservation hébergement - Forest",
+    description:
+      "Venez réserver votre séjour éco-responsable dans le Gers. Découvrez des hébergements écoresponsables.",
+  },
 };
 
 export default function RootLayout({
