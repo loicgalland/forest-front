92b3e7ad1734621feef47158e8cdaf7687909a09
loicgalland <loicgalland1@gmail.com>
2025-01-09 19:39:15 +0100

    feat(header): add logo to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,7 +9,7 @@ import UserIcon from "@/app/assets/images/svg/userIcon.svg";
 import UserIconWhite from "@/app/assets/images/svg/userIconWhite.svg";
 import BurgerMenu from "@/app/assets/images/svg/burgerMenu.svg";
 import CloseMenu from "@/app/assets/images/svg/menuClose.svg";
-import ForestIcon from "@/app/assets/images/svg/forest_logo.svg"
+import ForestIcon from "../assets/images/svg/forest_logo.svg";
 
 export function Header() {
   const [isMobile, setIsMobile] = useState<boolean>(true);
@@ -95,8 +95,8 @@ export function Header() {
       <header>
         <div className="md:px-20 lg:px-40 xl:px-80 flex justify-between items-center md:bg-beige h-[80px] font-bold text-xl font-happy z-10 relative">
           <div className="w-full relative flex justify-between items-center">
-            <Image src={ForestIcon} alt={"logo Forest"} width={100} height={100}></Image>
-            <h1 className="text-[30px] font-light uppercase py-2 px-4 md:px-0">
+            <h1 className="text-[30px] font-light uppercase py-2 px-4 md:px-0 flex items-center gap-2">
+              <ForestIcon width={50} height={50} />
               <Link
                 href="/home"
                 onClick={() => {
@@ -106,6 +106,7 @@ export function Header() {
                 Forest
               </Link>
             </h1>
+
             {isMobile ? (
               <button onClick={handleMenu} className="flex z-30 py-2 px-4">
                 {isMenuActive ? <CloseMenu /> : <BurgerMenu />}
