48cc3813398d43961b40688cee980ccd1d4102e2
loicgalland <loicgalland1@gmail.com>
2025-01-09 19:59:18 +0100

    feat(security): test locked branche

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -123,6 +123,7 @@ export function Header() {
                     : "hidden"
               }
             >
+              11
               <ul
                 className={
                   !isMobile
